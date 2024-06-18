e3d13fc14153a82b0ea93d96cb939a049c425126
Shashwat Panigrahi <shashwat.panigrahi@stonybrook.edu>
2024-06-17 21:59:10 -0400

    Initial commit

---
+++ app/layout.jsx
@@ -0,0 +1,25 @@
+import '@styles/globals.css'
+
+export const metadata = {
+    title: "Share_prompts",
+    description: "Discover and Share AI prompts"
+}
+
+const RootLayout = ({ children }) => {
+  return (
+    <html lang="en">
+        <body>
+            <div className="main">
+                <div className="gradient">   
+
+                </div>
+            </div>
+            <main className="app">
+                {children}
+            </main>
+        </body>
+    </html>
+  )
+}
+
+export default RootLayout;
